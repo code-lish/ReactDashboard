f5a57237855433c371fd2f77cd0baab56164c884
Noormohammad <n.rahemi1377@gmail.com>
2023-01-11 14:17:14 +0430

    Navbar folder changed to navbar with small n

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar/Sidebar";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../navbar/Navbar";
 import { Box, Container } from "@mui/material";
 
 const Layout = () => {
